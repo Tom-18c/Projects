44172ed68a7439021251c29da0a685aabe3cd2f7
Tom-18c <Tom18c@outlook.com>
2026-06-19 17:47:38 +0800

    20260619-用2种方法（切片法、滑动法）完成通过特征字符串截取主字符串的3种功能（取前、取后、取中间）

--- Feature Module/Mature Feature Module/Tom_Functions.py
+++ Feature Module/Mature Feature Module/Tom_Functions.py
@@ -1,8 +1,12 @@
+from typing import Union
+
+
 class Feature_OnlyStoreData_Str_Int:
     # 仅用作存储数据
     def __init__(self, tom_feature_str: str, tom_which_location: int = 1) -> None:
         self.tom_feature_str = tom_feature_str
         self.tom_which_location = tom_which_location
+        # self.tom_find_next = tom_find_next
 
 
 def tom_calculate_next_current(feature_str):
@@ -77,15 +81,16 @@ def tom_list_all_index(main_str, feature_str):
 
 
 class Tom_Fun_Extract:
-    def __init__(self, input_pending_str: str, input_feature_instance: Feature_OnlyStoreData_Str_Int):
+    def __init__(self, input_pending_str: str, input_feature_instance: Feature_OnlyStoreData_Str_Int, input_feature_instance2: Union[Feature_OnlyStoreData_Str_Int, None] = None):
         # 输入检查
         if not isinstance(input_pending_str, str):
-            raise TypeError(f"input_pending_val must be str, not {type(input_pending_str).__name__}")
+            raise TypeError(f"input_pending_str must be str, not {type(input_pending_str).__name__}")
         if not isinstance(input_feature_instance, Feature_OnlyStoreData_Str_Int):
-            raise TypeError(f"input_dict must be a dictionary, not {type(input_feature_instance).__name__}")
+            raise TypeError(f"input_feature_instance must be a Feature_OnlyStoreData_Str_Int's instance, not {type(input_feature_instance).__name__}")
 
         self.input_pending_str = input_pending_str
         self.input_feature_instance = input_feature_instance
+        self.input_feature_instance2 = input_feature_instance2
 
     def tom_before_slice(self):
         # 截取特征
@@ -94,9 +99,9 @@ class Tom_Fun_Extract:
 
         # 输入检查
         if not isinstance(feature_location, int):
-            raise TypeError(f"feature_start must be int, not {type(feature_location).__name__}")
+            raise TypeError(f"feature_location must be int, not {type(feature_location).__name__}")
         if feature_location <= 0:
-            raise ValueError(f"feature_start must be positive integer, not {feature_location}")
+            raise ValueError(f"feature_location must be positive integer, not {feature_location}")
 
         # 返回结果
         tom_return_str = f"{feature}".join(self.input_pending_str.split(feature)[:feature_location])
@@ -111,9 +116,9 @@ class Tom_Fun_Extract:
 
         # 输入检查
         if not isinstance(feature_location, int):
-            raise TypeError(f"feature_start must be int, not {type(feature_location).__name__}")
+            raise TypeError(f"feature_location must be int, not {type(feature_location).__name__}")
         if feature_location <= 0:
-            raise ValueError(f"feature_start must be positive integer, not {feature_location}")
+            raise ValueError(f"feature_location must be positive integer, not {feature_location}")
 
         # 返回结果
         return f"{feature}".join(self.input_pending_str.split(feature)[feature_location:])
@@ -125,9 +130,9 @@ class Tom_Fun_Extract:
 
         # 输入检查
         if not isinstance(feature_location, int):
-            raise TypeError(f"feature_start must be int, not {type(feature_location).__name__}")
+            raise TypeError(f"feature_location must be int, not {type(feature_location).__name__}")
         if feature_location <= 0:
-            raise ValueError(f"feature_start must be positive integer, not {feature_location}")
+            raise ValueError(f"feature_location must be positive integer, not {feature_location}")
 
         # 找出所有特征字符串出现的位置
         index_position = tom_list_all_index(self.input_pending_str, feature)
@@ -137,15 +142,16 @@ class Tom_Fun_Extract:
         return self.input_pending_str[: index_position[feature_location - 1]]
 
     def tom_after_slide(self):
+
         # 截取特征
         feature = str(self.input_feature_instance.tom_feature_str)
         feature_location = self.input_feature_instance.tom_which_location
 
         # 输入检查
         if not isinstance(feature_location, int):
-            raise TypeError(f"feature_start must be int, not {type(feature_location).__name__}")
+            raise TypeError(f"feature_location must be int, not {type(feature_location).__name__}")
         if feature_location <= 0:
-            raise ValueError(f"feature_start must be positive integer, not {feature_location}")
+            raise ValueError(f"feature_location must be positive integer, not {feature_location}")
 
         # 找出所有特征字符串出现的位置
         index_position = tom_list_all_index(self.input_pending_str, feature)
@@ -154,20 +160,63 @@ class Tom_Fun_Extract:
             return ""
         return self.input_pending_str[index_position[feature_location - 1] + len(feature) :]
 
+    def tom_between_slice(self):
+        # 输入检查
+        if not isinstance(self.input_feature_instance2, Feature_OnlyStoreData_Str_Int):
+            raise TypeError(f"input_feature_instance2 must be a Feature_OnlyStoreData_Str_Int's instance, not {type(self.input_feature_instance2).__name__}")
 
+        # 返回结果
+        # 掐头
+        tom_process_str = Tom_Fun_Extract(self.input_pending_str, self.input_feature_instance).tom_after_slice()
+        if tom_process_str == "":
+            return ""
+        # 去尾
+        tom_return_str = Tom_Fun_Extract(tom_process_str, self.input_feature_instance2).tom_before_slice()
+        return tom_return_str
+
+    def tom_between_slide(self):
+        # 输入检查
+        if not isinstance(self.input_feature_instance2, Feature_OnlyStoreData_Str_Int):
+            raise TypeError(f"input_feature_instance2 must be a Feature_OnlyStoreData_Str_Int's instance, not {type(self.input_feature_instance2).__name__}")
+
+        # 返回结果
+        # 掐头
+        tom_process_str = Tom_Fun_Extract(self.input_pending_str, self.input_feature_instance).tom_after_slide()
+        if tom_process_str == "":
+            return ""
+        # 去尾
+        tom_return_str = Tom_Fun_Extract(tom_process_str, self.input_feature_instance2).tom_before_slide()
+        return tom_return_str
+
+
+"""
 # 用法举例
-# any_str = "968888+888888888880>520≤2350≤2400≤2700<5555"
-# feature_1 = Feature_OnlyStoreData_Str_Int("<", 1)
-# feature_2 = Feature_OnlyStoreData_Str_Int("≤", 3)
-# feature_3 = Feature_OnlyStoreData_Str_Int("888", 3)
-# print("========================================================================================")
-# print(f"以第{feature_3.tom_feature_str}为特征，使用切片法，截取第{feature_3.tom_which_location}个特征前的所有内容，否则结果为空：")
-# print(Tom_Fun_Extract(any_str, feature_3).tom_before_slice())
-# print(f"以第{feature_3.tom_feature_str}为特征，使用滑动法，截取第{feature_3.tom_which_location}个特征前的所有内容，否则结果为空：")
-# print(Tom_Fun_Extract(any_str, feature_3).tom_before_slide())
-# print("========================================================================================")
-# print(f"以第{feature_3.tom_feature_str}为特征，使用切片法，截取第{feature_3.tom_which_location}个特征后的所有内容，否则结果为空：")
-# print(Tom_Fun_Extract(any_str, feature_3).tom_after_slice())
-# print(f"以第{feature_3.tom_feature_str}为特征，使用滑动法，截取第{feature_3.tom_which_location}个特征后的所有内容，否则结果为空：")
-# print(Tom_Fun_Extract(any_str, feature_3).tom_after_slide())
-# print("========================================================================================")
+any_str = "968888+888888888880>520≤2350≤2400≤2700<5555"
+feature_1 = Feature_OnlyStoreData_Str_Int("<", 1)
+feature_2 = Feature_OnlyStoreData_Str_Int("≤", 3)
+feature_3 = Feature_OnlyStoreData_Str_Int("888", 3)
+print("========================================================================================")
+print(f"以{feature_3.tom_feature_str}为特征，使用切片法，截取第{feature_3.tom_which_location}个特征前的所有内容，否则结果为空：")
+print(Tom_Fun_Extract(any_str, feature_3).tom_before_slice())
+print(f"以{feature_3.tom_feature_str}为特征，使用滑动法，截取第{feature_3.tom_which_location}个特征前的所有内容，否则结果为空：")
+print(Tom_Fun_Extract(any_str, feature_3).tom_before_slide())
+print("========================================================================================")
+print(f"以{feature_3.tom_feature_str}为特征，使用切片法，截取第{feature_3.tom_which_location}个特征后的所有内容，否则结果为空：")
+print(Tom_Fun_Extract(any_str, feature_3).tom_after_slice())
+print(f"以{feature_3.tom_feature_str}为特征，使用滑动法，截取第{feature_3.tom_which_location}个特征后的所有内容，否则结果为空：")
+print(Tom_Fun_Extract(any_str, feature_3).tom_after_slide())
+print("========================================================================================")
+
+# 用法举例2
+any_str2 = "hhh门扇配2100mm，门扇开孔距下口600mmm，门扇开孔距锁方200mm，门扇离地3公分"
+feature_11 = Feature_OnlyStoreData_Str_Int("开孔距下口", 1)
+feature_12 = Feature_OnlyStoreData_Str_Int("mm", 2)
+feature_13 = Feature_OnlyStoreData_Str_Int("门扇", 2)
+print("========================================================================================")
+print(Tom_Fun_Extract(any_str2, feature_11, feature_12).tom_between_slice())
+print(Tom_Fun_Extract(any_str2, feature_13, feature_12).tom_between_slice())
+print("========================================================================================")
+print(Tom_Fun_Extract(any_str2, feature_11, feature_12).tom_between_slide())
+print(Tom_Fun_Extract(any_str2, feature_13, feature_12).tom_between_slide())
+print("========================================================================================")
+"""
--- 调用测试.py
+++ 调用测试.py
@@ -11,19 +11,31 @@ if target_dir not in sys.path:
 from Tom_Functions import Tom_Fun_Extract, Feature_OnlyStoreData_Str_Int
 
 # 用法举例
-# 用法举例
 any_str = "968888+888888888880>520≤2350≤2400≤2700<5555"
 feature_1 = Feature_OnlyStoreData_Str_Int("<", 1)
 feature_2 = Feature_OnlyStoreData_Str_Int("≤", 3)
 feature_3 = Feature_OnlyStoreData_Str_Int("888", 3)
 print("========================================================================================")
-print(f"以第{feature_3.tom_feature_str}为特征，使用切片法，截取第{feature_3.tom_which_location}个特征前的所有内容，否则结果为空：")
+print(f"以{feature_3.tom_feature_str}为特征，使用切片法，截取第{feature_3.tom_which_location}个特征前的所有内容，否则结果为空：")
 print(Tom_Fun_Extract(any_str, feature_3).tom_before_slice())
-print(f"以第{feature_3.tom_feature_str}为特征，使用滑动法，截取第{feature_3.tom_which_location}个特征前的所有内容，否则结果为空：")
+print(f"以{feature_3.tom_feature_str}为特征，使用滑动法，截取第{feature_3.tom_which_location}个特征前的所有内容，否则结果为空：")
 print(Tom_Fun_Extract(any_str, feature_3).tom_before_slide())
 print("========================================================================================")
-print(f"以第{feature_3.tom_feature_str}为特征，使用切片法，截取第{feature_3.tom_which_location}个特征后的所有内容，否则结果为空：")
+print(f"以{feature_3.tom_feature_str}为特征，使用切片法，截取第{feature_3.tom_which_location}个特征后的所有内容，否则结果为空：")
 print(Tom_Fun_Extract(any_str, feature_3).tom_after_slice())
-print(f"以第{feature_3.tom_feature_str}为特征，使用滑动法，截取第{feature_3.tom_which_location}个特征后的所有内容，否则结果为空：")
+print(f"以{feature_3.tom_feature_str}为特征，使用滑动法，截取第{feature_3.tom_which_location}个特征后的所有内容，否则结果为空：")
 print(Tom_Fun_Extract(any_str, feature_3).tom_after_slide())
+print("========================================================================================")
+
+# 用法举例2
+any_str2 = "hhh门扇配2100mm，门扇开孔距下口600mmm，门扇开孔距锁方200mm，门扇离地3公分"
+feature_11 = Feature_OnlyStoreData_Str_Int("开孔距下口", 1)
+feature_12 = Feature_OnlyStoreData_Str_Int("mm", 2)
+feature_13 = Feature_OnlyStoreData_Str_Int("门扇", 2)
+print("========================================================================================")
+print(Tom_Fun_Extract(any_str2, feature_11, feature_12).tom_between_slice())
+print(Tom_Fun_Extract(any_str2, feature_13, feature_12).tom_between_slice())
+print("========================================================================================")
+print(Tom_Fun_Extract(any_str2, feature_11, feature_12).tom_between_slide())
+print(Tom_Fun_Extract(any_str2, feature_13, feature_12).tom_between_slide())
 print("========================================================================================")
